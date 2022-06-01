82708faea78d75e6b939f6826ba9eb0e768c21e8
DESKTOP-EOVF5KP\ctrob <ctrobinson2232@gmail.com>
2022-06-01 07:30:11 -0600

    build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import './App.css';
 import './pages/Pages.css';
-import Navbar from './components/Navbar';
 import { FrictionLoss, SmoothBore, CreateScene } from './pages';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { RecoilRoot } from 'recoil';
-import Scene2 from './delete/create/Scene2';
 
 function App() {
 	return (
